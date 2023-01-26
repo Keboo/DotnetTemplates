7f8fee28bbb5556e5b4e79a22b06b62240908911
Kevin Bost <kitokeboo@gmail.com>
2023-01-25 22:30:06 -0800

    Updating Main View model
    Adding NuGet.config

--- templates/WPF/WpfApp/WpfApp/MainWindowViewModel.cs
+++ templates/WPF/WpfApp/WpfApp/MainWindowViewModel.cs
@@ -9,6 +9,7 @@ public partial class MainWindowViewModel : ObservableObject
     //See: https://learn.microsoft.com/dotnet/communitytoolkit/mvvm/generators/observableproperty
     //and: https://learn.microsoft.com/windows/communitytoolkit/mvvm/observableobject
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(IncrementCountCommand))]
     private int _count;
     
     public MainWindowViewModel()
@@ -19,11 +20,13 @@ public partial class MainWindowViewModel : ObservableObject
     //This is using the source generators from CommunityToolkit.Mvvm to generate a RelayCommand
     //See: https://learn.microsoft.com/dotnet/communitytoolkit/mvvm/generators/relaycommand
     //and: https://learn.microsoft.com/windows/communitytoolkit/mvvm/relaycommand
-    [RelayCommand]
+    [RelayCommand(CanExecute = nameof(CanIncrementCount))]
     private void IncrementCount()
     {
         Count++;
     }
+
+    private bool CanIncrementCount() => Count < 5;
 
     [RelayCommand]
     private void ClearCount()
