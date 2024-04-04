fd7e49f7a3f72eb956339944d899eb016416daf0
Kevin Bost <kitokeboo@gmail.com>
2024-04-03 22:22:31 -0700

    Default project

---
+++ templates/Avalonia/AvaloniaSolution/SampleAvaloniaApplication/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SampleAvaloniaApplication.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
