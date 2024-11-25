4876f816b2dfd7105550da74dc46a1030297f7f4
Kevin Bost <kitokeboo@gmail.com>
2024-11-24 22:55:23 -0800

    WIP Avalonia updates

---
+++ templates/Avalonia/AvaloniaSolution/SampleAvaloniaApplication/Views/IAppShell.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleAvaloniaApplication.Views;
+
+public interface IAppShell
+{
+    Task NavigateAsync(NavigationRequest request);
+    Task PushAsync(NavigationRequest request);
+    Task<bool> PopAsync();
+
+    Task ShowDialogAsync(DialogRequest request);
+    Task<TResult> ShowDialogAsync<TResult>(DialogRequest<TResult> request);
+}
---
+++ templates/Avalonia/AvaloniaSolution/SampleAvaloniaApplication/Views/NavigationRequest.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia.Controls;
+
+namespace SampleAvaloniaApplication.Views;
+
+public class NavigationRequest
+{
+    private NavigationRequest(Type viewType)
+    {
+        ViewType = viewType;
+    }
+
+    public virtual Task LoadDataAsync(Control control)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Type ViewType { get; }
+    public bool StartNewScope { get; private set; }
+
+    //public static NavigationRequest LoginPage() => new(typeof(LoginView)) { StartNewScope = true };
+    
+    private class NavigationRequestData<T> : NavigationRequest
+    {
+        public NavigationRequestData(Type viewType, T data)
+            : base(viewType)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+
+        public override Task LoadDataAsync(Control control)
+        {
+            if (control is IDataView<T> dataView)
+            {
+                return dataView.LoadAsync(Data);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
+
+
